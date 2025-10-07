7825b723b53e715685aba9c10f14c9c4591afa75
ehsanKhan <iamehsanalaee006@gmail.com>
2025-10-07 15:30:33 +0330

    get template to project

---
+++ NewsBlogWebPage/Pages/Article.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewsBlogWebPage.Pages
+{
+    public class ArticleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NewsBlogWebPage/Pages/Category.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewsBlogWebPage.Pages
+{
+    public class CategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
